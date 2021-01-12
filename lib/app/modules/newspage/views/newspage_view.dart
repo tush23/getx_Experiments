a8a6bb472b4001b82a41a1f309bd49093eb1a6ec
tushardubey <dubey.tushar23@gmail.com>
2021-01-12 13:14:54 +0530

    ui changes

--- lib/app/modules/newspage/views/newspage_view.dart
+++ lib/app/modules/newspage/views/newspage_view.dart
@@ -9,27 +9,19 @@ class NewsPageView extends GetView<NewspageController> {
   Widget build(BuildContext context) {
     // _newController.getNew();
     // _newController.getNewData();
-    // controller.getNewData();
+    controller.getNewData();
     return Scaffold(
       appBar: AppBar(
         title: Text('News'),
         centerTitle: true,
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: controller.obx(
             (state) => Column(
-                  children: [
-                    Image.network(controller.data.value.articles[0].urlToImage),
-                    Text(controller.data.value.articles[0].title ?? '',
-                        style: TextStyle(fontSize: 24)),
-                    Text(
-                      controller.data.value.articles[0].description ?? '',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    Text(controller.data.value.articles[0].author ?? '',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold)),
-                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: List<Widget>.generate(
+                      controller.data.value.articles.length,
+                      (index) => articelCard(index)),
                 ),
             onError: (error) => Text('Somthing went wrong $error')),
         // child: Obx(() => Text(
@@ -37,6 +29,26 @@ class NewsPageView extends GetView<NewspageController> {
         //       style: TextStyle(fontSize: 20),
         //     )),
       ),
+    );
+  }
+
+  Widget articelCard(int i) {
+    return Column(
+      children: [
+        Image.network(controller.data.value.articles[i].urlToImage ??
+            'https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-260nw-1037719192.jpg'),
+        Text(controller.data.value.articles[i].title ?? '',
+            style: TextStyle(fontSize: 20)),
+        Text(
+          controller.data.value.articles[i].description ?? '',
+          style: TextStyle(fontSize: 17),
+        ),
+        Text(controller.data.value.articles[i].author ?? '',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        SizedBox(
+          height: 5,
+        )
+      ],
     );
   }
 }
