082c9a48c2777ada2b90bc020a087f91312f2fcb
tushardubey <dubey.tushar23@gmail.com>
2021-01-12 13:44:04 +0530

    format 1

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,14 +1,3 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
-  @override
-  void onInit() {}
-  @override
-  void onReady() {}
-  @override
-  void onClose() {}
-  void increment() => count.value++;
-}
+class HomeController extends GetxController {}
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,8 +3,8 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   static const HOME = _Paths.HOME;
-  static const LOGIN = _Paths.LOGIN;
-  static const CART = _Paths.CART;
+  // static const LOGIN = _Paths.LOGIN;
+  // static const CART = _Paths.CART;
   // static const PROFILE = _Paths.PROFILE;
   // static const PAGE2 = _Paths.PAGE2;
   static const NEWSPAGE = _Paths.NEWSPAGE;
@@ -13,9 +13,6 @@ abstract class Routes {
 
 abstract class _Paths {
   static const HOME = '/home';
-  static const LOGIN = '/login';
-  static const CART = '/cart';
-  static const PAGE2 = '/page2';
   static const NEWSPAGE = '/newspage';
   // static const PAGE2 = '/page2';
 }
