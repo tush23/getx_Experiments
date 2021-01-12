56245255bc4c460e11e2edd8584164b360fed1ed
tushardubey <dubey.tushar23@gmail.com>
2021-01-12 13:42:52 +0530

    farmat

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,8 +4,8 @@ import 'package:getx_cli/app/modules/home/bindings/home_binding.dart';
 import 'package:getx_cli/app/modules/home/views/home_view.dart';
 import 'package:getx_cli/app/modules/newspage/bindings/newspage_binding.dart';
 import 'package:getx_cli/app/modules/newspage/views/newspage_view.dart';
-import 'package:getx_cli/app/modules/page2/bindings/page2_binding.dart';
-import 'package:getx_cli/app/modules/page2/views/page2_view.dart';
+// import 'package:getx_cli/app/modules/page2/bindings/page2_binding.dart';
+// import 'package:getx_cli/app/modules/page2/views/page2_view.dart';
 
 // import 'package:getx_cli/app/modules/cart/bindings/cart_binding.dart';
 // import 'package:getx_cli/app/modules/cart/views/cart_view.dart';
@@ -50,10 +50,10 @@ class AppPages {
       page: () => NewsPageView(),
       binding: NewspageBinding(),
     ),
-    GetPage(
-      name: _Paths.PAGE2,
-      page: () => Page2View(),
-      binding: Page2Binding(),
-    ),
+    // GetPage(
+    //   name: _Paths.PAGE2,
+    //   page: () => Page2View(),
+    //   binding: Page2Binding(),
+    // ),
   ];
 }
