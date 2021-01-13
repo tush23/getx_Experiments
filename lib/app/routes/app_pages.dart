7f4d9bc2f3a4a45bd0c08aa4a58aacf9d48ca222
tushardubey <dubey.tushar23@gmail.com>
2021-01-13 23:03:13 +0530

    just for prof 4

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,18 +4,7 @@ import 'package:getx_cli/app/modules/home/bindings/home_binding.dart';
 import 'package:getx_cli/app/modules/home/views/home_view.dart';
 import 'package:getx_cli/app/modules/newspage/bindings/newspage_binding.dart';
 import 'package:getx_cli/app/modules/newspage/views/newspage_view.dart';
-// import 'package:getx_cli/app/modules/page2/bindings/page2_binding.dart';
-// import 'package:getx_cli/app/modules/page2/views/page2_view.dart';
 
-// import 'package:getx_cli/app/modules/cart/bindings/cart_binding.dart';
-// import 'package:getx_cli/app/modules/cart/views/cart_view.dart';
-// import 'package:getx_cli/app/modules/login/bindings/login_binding.dart';
-// import 'package:getx_cli/app/modules/login/views/login_view.dart';
-// import 'package:getx_cli/app/modules/page2/bindings/page2_binding.dart';
-// import 'package:getx_cli/app/modules/page2/views/page2_view.dart';
-
-// import 'package:getx_cli/app/modules/wishlist/bindings/wishlist_binding.dart';
-// import 'package:getx_cli/app/modules/wishlist/views/wishlist_view.dart';
 
 part 'app_routes.dart';
 
@@ -28,22 +17,7 @@ class AppPages {
       page: () => HomeView(),
       binding: HomeBinding(),
     ),
-    // GetPage(
-    //   name: _Paths.LOGIN,
-    //   page: () => LoginView(),
-    //   binding: LoginBinding(),
-    // ),
-
-    // GetPage(
-    //   name: _Paths.CART,
-    //   page: () => CartView(),
-    //   binding: CartBinding(),
-    // ),
-    // GetPage(
-    //   name: _Paths.WISHLIST,
-    //   page: () => WishlistView(),
-    //   binding: WishlistBinding(),
-    // ),
+   
 
     GetPage(
       name: _Paths.NEWSPAGE,
