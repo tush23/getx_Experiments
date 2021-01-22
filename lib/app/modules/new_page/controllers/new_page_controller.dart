de11aeb2251679fd2061fd7e02c50a757eecd37f
tushardubey <dubey.tushar23@gmail.com>
2021-01-22 12:29:03 +0530

    removed commets

--- lib/app/modules/new_page/controllers/new_page_controller.dart
+++ lib/app/modules/new_page/controllers/new_page_controller.dart
@@ -2,12 +2,5 @@ import 'package:get/get.dart';
 
 class NewPageController extends GetxController {
 
-  // final count = 0.obs;
-  // @override
-  // void onInit() {}
-  // @override
-  // void onReady() {}
-  // @override
-  // void onClose() {}
-  // void increment() => count.value++;
+  
 }
