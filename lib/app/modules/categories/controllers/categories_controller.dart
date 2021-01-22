7f546b74aa2d0eac941ddfd43cd9227124349fd1
tushardubey <dubey.tushar23@gmail.com>
2021-01-22 15:06:31 +0530

    cat changes

--- lib/app/modules/categories/controllers/categories_controller.dart
+++ lib/app/modules/categories/controllers/categories_controller.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
 
 class CategoriesController extends GetxController {
-  //TODO: Implement CategoriesController
 
   final count = 0.obs;
   @override
-  void onInit() {}
+  void onInit() {
+    
+  }
   @override
   void onReady() {}
   @override
