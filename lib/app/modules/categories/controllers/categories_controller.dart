63d197645edf3c30a6ecdeb23f740e21dbf60272
tushardubey <dubey.tushar23@gmail.com>
2021-01-22 15:06:47 +0530

    worked

--- lib/app/modules/categories/controllers/categories_controller.dart
+++ lib/app/modules/categories/controllers/categories_controller.dart
@@ -1,14 +1,16 @@
 import 'package:get/get.dart';
 
 class CategoriesController extends GetxController {
-
   final count = 0.obs;
   @override
   void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
     
   }
-  @override
-  void onReady() {}
   @override
   void onClose() {}
   void increment() => count.value++;
