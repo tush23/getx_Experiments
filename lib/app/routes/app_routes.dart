f166025477261ffe5e2c44e28e91122aff0f9ad3
tushardubey <dubey.tushar23@gmail.com>
2021-01-31 18:05:59 +0530

    removed1.1

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,13 +3,9 @@ part of 'app_pages.dart';
 abstract class Routes {
   static const HOME = _Paths.HOME;
   static const NEWSPAGE = _Paths.NEWSPAGE;
-  static const NEW_PAGE = _Paths.NEW_PAGE;
-  static const CATEGORIES = _Paths.CATEGORIES;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
   static const NEWSPAGE = '/newspage';
-  static const NEW_PAGE = '/new-page';
-  static const CATEGORIES = '/categories';
 }
