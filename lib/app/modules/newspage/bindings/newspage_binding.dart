fb091c921623df03a56bbba5464bdfdadfb68886
tushardubey <dubey.tushar23@gmail.com>
2021-01-31 18:42:51 +0530

    bug fix

--- lib/app/modules/newspage/bindings/newspage_binding.dart
+++ lib/app/modules/newspage/bindings/newspage_binding.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 import 'package:getx_cli/app/attemted_provider/api_provider.dart';
 import 'package:getx_cli/app/modules/newspage/controllers/newpagecon_with_api_provider.dart';
 
-import 'package:getx_cli/app/modules/newspage/controllers/newspage_controller.dart';
-
 class NewspageBinding extends Bindings {
   @override
   void dependencies() {
