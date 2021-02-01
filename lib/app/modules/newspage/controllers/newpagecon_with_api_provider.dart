ce4c196561e39e50832ef165acbe11e3e4c852d6
tushardubey <dubey.tushar23@gmail.com>
2021-02-01 17:36:58 +0530

    bug fix

--- lib/app/modules/newspage/controllers/newpagecon_with_api_provider.dart
+++ lib/app/modules/newspage/controllers/newpagecon_with_api_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:getx_cli/app/customs/handler.dart';
