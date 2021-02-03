120b0397efe1104f9dde623772812b5e14fe60b9
tushardubey <dubey.tushar23@gmail.com>
2021-02-03 15:56:35 +0530

    small but not improved

--- lib/app/modules/search/bindings/search_binding.dart
+++ lib/app/modules/search/bindings/search_binding.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 import 'package:getx_cli/app/modules/search/controllers/search_controller.dart';
 import 'package:getx_cli/app/provider/api_provider.dart';
 
+
+
 class SearchBinding extends Bindings {
   @override
   void dependencies() {
